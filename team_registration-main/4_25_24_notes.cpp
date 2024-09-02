09a5144945f5995a2b3cf6e393ceed334dfd7ade
Danny <139923108+Danny-Yu-X@users.noreply.github.com>
2024-09-01 21:25:55 -0700

    Add files via upload

---
+++ team_registration-main/4_25_24_notes.cpp
@@ -0,0 +1,13 @@
+/*
+high-level design of hw2 part 2
+1. check whether we indeed have three arguments
+2. File(s) --> JSONs (JSON::value) [ecs36b_JSON.cpp -- myFile2Json() -- check whether it is a valid JSON from sytax]
+3. Check whether the JSON is valid for HW 2
+array, each element 2 key-value pairs, GPS_DD and Time, GPS_DD --> latitude and longitude
+
+//JSON and C++/Java
+
+
+
+
+*/
